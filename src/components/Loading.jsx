2b005f14401ab8c10da8af186947bf96510bafc3
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-02-03 22:25:39 +0530

    Added the sidebar and the buttons

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -11,7 +11,7 @@ function LoadingComponent() {
     return (
         <Box as="section" w="full" h="100vh" position={'fixed'} top={0} zIndex={'1000'} display="flex" alignItems="center" background={'rgba(0, 0, 0, 0.75)'} justifyContent="center" transition={'all 0.5s ease-in-out'}>
             <Center py={6} px={8} background={'white'} rounded="md" shadow="md" display={'flex'} flexDir={'column'} gap={'2'}>
-                <Spinner color='#8141bb' thickness="3px" size={'xl'} emptyColor='purple.100' />
+                <Spinner color='#5340ff' thickness="3px" size={'xl'} emptyColor='rgba(83, 64, 255,0.4)' />
                 <Text fontWeight={'semibold'}>Loading</Text>
             </Center>
         </Box>
--- src/pages/Layout/Header.jsx
+++ src/pages/Layout/Header.jsx
@@ -1,23 +1,30 @@
-import { Box, Button, HStack, Image, Text } from '@chakra-ui/react'
+import { Box, Button, Drawer, DrawerBody, DrawerContent, DrawerFooter, DrawerHeader, DrawerOverlay, HStack, Image, Menu, MenuGroup, MenuItem, Text, VStack, useDisclosure } from '@chakra-ui/react'
 import React from 'react'
 import { Link, useLocation } from 'react-router-dom'
 import logo from '../../assets/logo.png'
 import { headerLinks } from '../../data'
-import { MdMail } from 'react-icons/md'
+import { MdMail, MdOutlineInfo, MdOutlineMenu } from 'react-icons/md'
 import { BiSolidPhoneCall as IoCall } from 'react-icons/bi'
 import toast from 'react-hot-toast'
+import { GrClose } from 'react-icons/gr'
+import { AiOutlineHome } from 'react-icons/ai'
+import { HiMenuAlt1 } from 'react-icons/hi'
+import { FaChalkboardTeacher } from 'react-icons/fa'
 
 const Header = () => {
   const location = useLocation();
   return (
     <>
-      <Box boxShadow={'sm'} py={'4'} px={'6'} display={['flex']} alignItems={['center']} justifyContent={['space-around']} zIndex={1000} position={'fixed'} width={'full'} top={0} >
-
+      <Box boxShadow={'sm'} py={'4'} px={'6'} display={['flex']} alignItems={['center']} justifyContent={['space-between', 'space-between', 'space-around', 'space-around']} zIndex={1000} position={'fixed'} width={'full'} top={0} >
 
         <NavLogo logo={logo} />
         <NavLinks location={location} />
         <NavButton />
+        <HamburgerButton logo={logo} />
       </Box>
+
+
+
     </>
   )
 }
@@ -47,13 +54,14 @@ const NavLinks = React.memo(({ location }) => {
     }
   </Box>
 });
+
 const NavButton = () => {
   const copyNumber = () => {
     navigator.clipboard.writeText('9584412188');
     toast.success("Number Copied to Clipboard")
   }
   return (<>
-    <HStack>
+    <HStack display={['none', 'none', 'flex', 'flex']} gap={4}>
       <Button fontSize={'sm'} className='navButton' >
         <Link to={'/contact'} ><HStack><MdMail /><span>Contact</span></HStack></Link>
       </Button>
@@ -61,6 +69,58 @@ const NavButton = () => {
     </HStack>
   </>
   );
+}
+
+const HamburgerButton = ({ logo }) => {
+  const { isOpen: isDrawerOpen, onOpen: onDrawerOpen, onClose: onDrawerClose } = useDisclosure();
+  const btnRef = React.useRef();
+
+  const copyNumber = () => {
+    navigator.clipboard.writeText('9584412188');
+    toast.success("Number Copied to Clipboard")
+  }
+  return (
+    <>
+      <Button ref={btnRef} display={['block', 'block', 'none', 'none']} onClick={onDrawerOpen} className='navButton'><MdOutlineMenu size={'20'} /></Button>
+
+
+      <Drawer finalFocusRef={btnRef} placement="left" isOpen={isDrawerOpen} onClose={onDrawerClose}>
+        <DrawerOverlay backdropFilter={'blur(3px)'} />
+        <DrawerContent>
+          <DrawerHeader borderBottomWidth={'2px'}>
+            <Link to={'/'} onClick={onDrawerClose}>
+              <Box display={['flex']} alignItems={'center'} justifyContent={'flex-start'} >
+                <Image width={'10'} src={logo} borderRadius={'full'} dropShadow={'0px 0px 10px #f9c307'} />
+                <Text fontWeight={'bold'} fontSize={'1.2rem'} ml={'2'} color={'#5340ff'} >Proton Education</Text>
+              </Box>
+            </Link>
+            <Button colorScheme='gray' size={'sm'} position={'absolute'} right={'5'} top={'4'} onClick={onDrawerClose} >
+              <GrClose />
+            </Button>
+          </DrawerHeader>
+
+          <DrawerBody >
+            <Menu >
+              <MenuGroup>
+                <MenuItem fontSize={'sm'} onClick={onDrawerClose} _hover={{ bg: "rgba(83, 64, 255,0.2)", color: 'rgb(83,64,255)' }} gap={'2'}><AiOutlineHome /><Link className='width-full' to={'/'}> Home</Link></MenuItem>
+                <MenuItem fontSize={'sm'} onClick={onDrawerClose} _hover={{ bg: "rgba(83, 64, 255,0.2)", color: 'rgb(83,64,255)' }} gap={'2'}><MdOutlineInfo /> <Link className='width-full' to={'/about'}>About</Link></MenuItem>
+                <MenuItem fontSize={'sm'} onClick={onDrawerClose} _hover={{ bg: "rgba(83, 64, 255,0.2)", color: 'rgb(83,64,255)' }} gap={'2'}><HiMenuAlt1 /> <Link className='width-full' to={'/facility'}>Facility</Link></MenuItem>
+                <MenuItem fontSize={'sm'} onClick={onDrawerClose} _hover={{ bg: "rgba(83, 64, 255,0.2)", color: 'rgb(83,64,255)' }} gap={'2'}><FaChalkboardTeacher /> <Link className='width-full' to={'/faculty'}>Faculty</Link></MenuItem>
+              </MenuGroup>
+            </Menu>
+          </DrawerBody>
+          <DrawerFooter>
+              <HStack display={'flex'} gap={4}>
+              <Button fontSize={'sm'} className='navButton' onClick={onDrawerClose} >
+                <Link to={'/contact'} ><HStack><MdMail /><span>Contact</span></HStack></Link>
+              </Button>
+              <Button onClick={copyNumber} fontSize={'sm'}><HStack><IoCall /><span>Call Now</span></HStack></Button>
+              </HStack>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </>
+  )
 }
 
 export default Header;
