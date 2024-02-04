805b3deafe5b7c513f44abaf33de8b14ff47c5d4
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-02-04 18:08:26 +0530

    Did the backend Integration

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,38 +1,84 @@
+import { Box, Button, Divider, Heading, Input, InputGroup, InputLeftElement, ListItem, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Text, Textarea, UnorderedList, VStack, useDisclosure } from '@chakra-ui/react'
+import axios from 'axios'
 import React, { useState } from 'react'
-import TransitionWrapper from '../components/Transition'
-import MainWrapper from '../components/MainWrapper'
-import { Box, Button, Divider, HStack, Heading, Input, InputGroup, InputLeftElement, ListItem, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, PinInput, PinInputField, Text, Textarea, UnorderedList, VStack, useDisclosure } from '@chakra-ui/react'
-import { AiFillMail, AiOutlineMail, AiOutlineUser } from 'react-icons/ai'
-import { MdClose, MdLockOpen, MdSend } from 'react-icons/md'
-import { BiSolidSend } from 'react-icons/bi'
 import toast from 'react-hot-toast'
-import { FaUnlock } from 'react-icons/fa'
+import { AiOutlineMail, AiOutlineUser } from 'react-icons/ai'
+import { BiSolidSend } from 'react-icons/bi'
+import { MdClose, MdLockOpen } from 'react-icons/md'
+import MainWrapper from '../components/MainWrapper'
+import TransitionWrapper from '../components/Transition'
 
 const Contact = () => {
-    const [email, setEmail] = useState("");
-    const [message, setMessage] = useState("");
     const [name, setName] = useState("");
-    const [phoneNumber, setPhoneNumber] = useState();
+    const [email, setEmail] = useState("");
+    const [phoneNumber, setPhoneNumber] = useState(0);
+    const [message, setMessage] = useState("");
+    const [isClicked, setIsClicked] = useState(false);
 
     const [otp, setOtp] = useState();
     const [verEmail, setVerEmail] = useState("");
 
-    const {isOpen, onOpen, onClose} = useDisclosure();
+    const { isOpen, onOpen, onClose } = useDisclosure();
 
-    const sendOTP = () => {
-        console.log(email, message, name, phoneNumber);
-        onOpen();
-        toast.success("OTP sent to your email. Please check your email and fill the OTP.", {
-            duration: 5000,
-            position: 'top-center',
-        })
-    }
-    const verifyOtp = () => {
-        console.log(otp, verEmail);
-        toast.success("Your query has been posted. We'll get in touch with you asap.", {
-            duration: 5000,
-            position: 'top-center',
-        })
+    const sendOTP = async (e) => {
+        e.preventDefault();
+        setIsClicked(true);
+        try {
+            console.log({name, email, phoneNumber, message});
+            const res = await axios.post('https://proton-education-server.onrender.com/sendmail', {
+                name: name,
+                email: email,
+                phoneNumber: Number(phoneNumber),
+                message: message,
+            });
+
+            console.log(res.data);
+
+            if (res.data.success) {
+                toast.success('OTP sent successfully. Please check your email.',{
+                    duration: 5000,
+                    position: "top-center",
+                });
+                onOpen();
+                setIsClicked(false);
+                setEmail('');
+                setName('');
+                setPhoneNumber('');
+                setMessage('');
+            } else {
+                toast.error('Failed to send OTP. Please try again later.');
+                setIsClicked(false);
+            }
+        } catch (error) {
+            console.error('Error sending OTP:', error);
+            toast.error('An error occurred. Please try again later.');
+            setIsClicked(false);
+        }
+    };
+
+
+    const verifyOtp = async() => {
+        setIsClicked(true);
+        const res = await axios.post('https://proton-education-server.onrender.com/verifyotp', {
+            email: verEmail,
+            otp: Number(otp),
+        });
+
+        if(res.data.success){
+            if(res.data.success){
+                toast.success('Query sent successfully. We\'ll get in touch with you asap.')
+                onClose();
+                setIsClicked(false);
+                setVerEmail('');
+                setOtp('');
+            }else{
+                toast.error('Failed to send OTP. Please try again later.');
+                setIsClicked(false);
+            }
+        }else{
+            toast.error('Invalid OTP. Please try again.')
+            setIsClicked(false);
+        }
     }
 
     return (
@@ -46,24 +92,29 @@ const Contact = () => {
                             <InputLeftElement pointerEvents={'none'}>
                                 <AiOutlineUser size='18' />
                             </InputLeftElement>
-                            <Input type='text' placeholder='John Doe' focusBorderColor='#5340ff' fontSize={'sm'} required={true} onChange={(e) => setName(e.target.value)} />
+                            <Input isRequired={true} type='text' placeholder='John Doe' focusBorderColor='#5340ff' fontSize={'sm'} onChange={(e) => setName(e.target.value)} />
                         </InputGroup>
                         <InputGroup spacing='4' >
                             <InputLeftElement pointerEvents={'none'}>
                                 <AiOutlineMail size='18' />
                             </InputLeftElement>
-                            <Input type='email' placeholder='johndoe@gmail.com' focusBorderColor='#5340ff' fontSize={'sm'} required={true} onChange={(e) => setEmail(e.target.value)} />
+                            <Input type='email' placeholder='johndoe@gmail.com' focusBorderColor='#5340ff' fontSize={'sm'} isRequired={true} onChange={(e) => setEmail(e.target.value)} />
                         </InputGroup>
                         <InputGroup spacing='4' >
                             <InputLeftElement pointerEvents={'none'}>
                                 <AiOutlineMail size='18' />
                             </InputLeftElement>
-                            <Input type='number' placeholder='961758XXXX' focusBorderColor='#5340ff' fontSize={'sm'} required={true} onChange={(e) => setPhoneNumber(e.target.value)} />
+                            <Input type='number' placeholder='961758XXXX' focusBorderColor='#5340ff' fontSize={'sm'} isRequired={true} onChange={(e) => setPhoneNumber(e.target.value)} />
                         </InputGroup>
 
-                        <Textarea placeholder='Your message for us.' fontSize={'sm'} resize={'none'} focusBorderColor='#5340ff' onChange={(e) => setMessage(e.target.value)} />
+                        <Textarea placeholder='Your message for us.' fontSize={'sm'} resize={'none'} focusBorderColor='#5340ff' isRequired={true} onChange={(e) => setMessage(e.target.value)} />
 
-                        <Button isDisabled={!name || !email || !phoneNumber || !message || phoneNumber.length > 10 || phoneNumber.length < 10 ? true:false} onClick={sendOTP} gap={2} className='navButton' variant='solid' fontSize={'sm'} width={'full'}>Send   <BiSolidSend /></Button>
+                        {
+                            isClicked ? 
+                                <Button isLoading onClick={e => sendOTP(e)} gap={2} className='navButton' variant='solid' fontSize={'sm'} width={'full'}>Send  <BiSolidSend /></Button>
+                             : 
+                                <Button isDisabled={!name || !email || !phoneNumber || !message || phoneNumber.length > 10 || phoneNumber.length < 10 ? true : false} onClick={e => sendOTP(e)} gap={2} className='navButton' variant='solid' fontSize={'sm'} width={'full'}>Send  <BiSolidSend /></Button>
+                        }
 
 
                         <Box
@@ -91,7 +142,7 @@ const Contact = () => {
 
 
             <Modal isOpen={isOpen} onClose={onClose} isCentered>
-                    <ModalOverlay />
+                <ModalOverlay />
                 <ModalContent>
                     <ModalHeader>
                         <Text color={"#5340ff"} fontSize={'2xl'} fontWeight={'bold'}>OTP Verification</Text>
@@ -105,20 +156,25 @@ const Contact = () => {
                             <InputLeftElement pointerEvents={'none'}>
                                 <AiOutlineMail size='18' />
                             </InputLeftElement>
-                            <Input type='email' placeholder='johndoe@gmail.com' focusBorderColor='#5340ff' fontSize={'sm'} required={true} onChange={(e) => setVerEmail(e.target.value)} />
+                            <Input type='email' placeholder='johndoe@gmail.com' focusBorderColor='#5340ff' fontSize={'sm'} isRequired={true} onChange={(e) => setVerEmail(e.target.value)} />
                         </InputGroup>
 
                         <InputGroup spacing='4' my={3} >
                             <InputLeftElement pointerEvents={'none'}>
                                 <MdLockOpen size='18' />
                             </InputLeftElement>
-                            <Input type='number' placeholder='452126' focusBorderColor='#5340ff' fontSize={'sm'} required={true} onChange={(e) => setOtp(e.target.value)} />
+                            <Input type='number' placeholder='452126' focusBorderColor='#5340ff' fontSize={'sm'} isRequired={true} onChange={(e) => setOtp(e.target.value)} />
                         </InputGroup>
 
                     </ModalBody>
 
                     <ModalFooter gap={2}>
-                        <Button isDisabled={!verEmail || !otp || otp.length > 6 || otp.length < 6 ? true : false} onClick={verifyOtp} gap={2} className='navButton' variant='solid' fontSize={'sm'} width={'50%'}>Post Query   <BiSolidSend /></Button>
+                        {
+                            isClicked ?
+                                <Button isLoading isDisabled={!verEmail || !otp || otp.length > 6 || otp.length < 6 ? true : false} onClick={verifyOtp} gap={2} className='navButton' variant='solid' fontSize={'sm'} width={'50%'}>Post Query   <BiSolidSend /></Button>
+                            : 
+                                <Button isDisabled={!verEmail || !otp || otp.length > 6 || otp.length < 6 ? true : false} onClick={verifyOtp} gap={2} className='navButton' variant='solid' fontSize={'sm'} width={'50%'}>Post Query   <BiSolidSend /></Button>
+                        }
                         <Button onClick={onClose} gap={2} fontSize={'sm'} width={'50%'}>Close <MdClose size={'18'} /></Button>
                     </ModalFooter>
                 </ModalContent>
